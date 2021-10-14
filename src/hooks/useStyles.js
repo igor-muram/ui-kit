0e6973e69341088106fd1079ca6da1b2c45c917c
IgorMuramshchikov <igor.muram@gmail.com>
2021-10-14 14:28:58 +0700

    Create useStyles hook

---
+++ src/hooks/useStyles.js
@@ -0,0 +1,39 @@
+import { computed } from 'vue';
+
+const types = {
+  default: {
+    size: 24,
+  },
+  big: {
+    size: 48,
+  },
+};
+
+export default function useStyles(icon) {
+  const defaults = computed(() => {
+    return types[icon.type] || types.default;
+  });
+
+  const sizeValue = computed(() => {
+    return icon.size || defaults.value.size;
+  });
+
+  const viewboxValue = computed(() => {
+    return `0 0 ${sizeValue.value} ${sizeValue.value}`;
+  });
+
+  const styles = computed(() => {
+    return {
+      '--scale-x': ['both', 'horizontal'].includes(icon.flip) ? '-1' : '1',
+      '--scale-y': ['both', 'vertical'].includes(icon.flip) ? '-1' : '1',
+      '--rotate': isNaN(icon.rotate) ? icon.rotate : icon.rotate + 'deg',
+      '--size': sizeValue.value,
+    };
+  });
+
+  return {
+    sizeValue,
+    viewboxValue,
+    styles,
+  };
+}
